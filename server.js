851833b582e53ccc190a331aa373ab030c65d563
lz1998 <875543533@qq.com>
2021-11-11 13:17:45 +0800

    fix memory leak

--- server.js
+++ server.js
@@ -16,37 +16,40 @@ require('echarts-themes-js/src/walden')
 require('echarts-themes-js/src/westeros')
 require('echarts-themes-js/src/wonderland')
 
+const canvas = createCanvas(1024, 768)
+
 // 生成图片
 function generateImage(options, width, height, theme) {
-    console.log("GENERATE IMAGE")
-    console.log(`WIDTH: ${width}`)
-    console.log(`HEIGHT: ${height}`)
-    console.log(`THEME: ${theme}`)
-    console.log(`OPTIONS: ${JSON.stringify(options)}`)
-    const canvas = createCanvas(width, height)
-    const ctx = canvas.getContext('2d')
-    ctx.font = '12px'
-    echarts.setCanvasCreator(() => canvas)
-    const chart = echarts.init(canvas, theme)
-    options.animation = false
-    chart.setOption(options)
-    return chart.getDom().toBuffer()
+  console.log("GENERATE IMAGE")
+  console.log(`WIDTH: ${width}`)
+  console.log(`HEIGHT: ${height}`)
+  console.log(`THEME: ${theme}`)
+  console.log(`OPTIONS: ${JSON.stringify(options)}`)
+  canvas.height = height
+  canvas.width = width
+  const ctx = canvas.getContext('2d')
+  ctx.font = '12px'
+  echarts.setCanvasCreator(() => canvas)
+  const chart = echarts.init(canvas, theme)
+  options.animation = false
+  chart.setOption(options)
+  return chart.getDom().toBuffer()
 }
 
 const express = require('express')
 const app = express()
 app.use(express.json())
 const imgController = (req, res) => {
-    console.log("RECV REQ")
-    let options = req.query.options || req.body
-    if (typeof options === 'string') {
-        options = JSON.parse(options)
-    }
-    let width = parseInt(req.query.width || options.width || 1024)
-    let height = parseInt(req.query.height || options.height || 768)
-    let theme = req.query.theme || options.theme || 'dark'
-    res.header("Content-Type", "image/png")
-    res.send(generateImage(options, width, height, theme))
+  console.log("RECV REQ")
+  let options = req.query.options || req.body
+  if (typeof options === 'string') {
+    options = JSON.parse(options)
+  }
+  let width = parseInt(req.query.width || options.width || 1024)
+  let height = parseInt(req.query.height || options.height || 768)
+  let theme = req.query.theme || options.theme || 'dark'
+  res.header("Content-Type", "image/png")
+  res.send(generateImage(options, width, height, theme))
 }
 app.post('/', imgController)
 app.get('/', imgController)
